fcab57569b6cda5dd21b80664c1347825c19b7a6
Anurag-Spec <79415746+Anurag-Spec@users.noreply.github.com>
2021-07-20 03:58:48 +0530

    gettingdata

--- frontend/src/Components/Carousel/Carousel.js
+++ frontend/src/Components/Carousel/Carousel.js
@@ -1,7 +1,22 @@
-import React from "react";
+import { React, useEffect, useState } from "react";
+import axios from "axios";
 
 function Carousel() {
-  return <div></div>;
+  const [imageData, setImageData] = useState([]);
+
+  useEffect(() => {
+    const getImageData = async () => {
+      const { data } = await axios.get("api/products");
+      setImageData(data);
+    };
+    getImageData();
+  }, []);
+
+  return (
+    <div>
+      <img src={imageData.products[0]} alt="hero-image" />
+    </div>
+  );
 }
 
 export default Carousel;
