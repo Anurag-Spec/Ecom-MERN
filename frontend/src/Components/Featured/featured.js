645e93ad209fd57a76bfacf75f7edb750b814f78
Anurag-Spec <79415746+Anurag-Spec@users.noreply.github.com>
2021-07-21 03:21:53 +0530

    added-products-featured

--- frontend/src/Components/Featured/featured.js
+++ frontend/src/Components/Featured/featured.js
@@ -1,13 +1,25 @@
-import React from "react";
+import { React, useEffect } from "react";
+import { useSelector } from "react-redux";
 
 function Featured() {
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
+
   return (
     <div>
-      <h3 className="featured-title">Featured Products</h3>
-      <img className="featured-image" src="#" alt="" />
-      <img className="featured-image" src="#" alt="" />
-      <img className="featured-image" src="#" alt="" />
-      <button className="featured-btn">Show All</button>
+      {loading ? (
+        <div>Loading</div>
+      ) : error ? (
+        <div>{error}</div>
+      ) : (
+        <div>
+          <h3 className="featured-title">Featured Products</h3>
+          <img className="featured-image" src={products[0].image} alt="" />
+          <img className="featured-image" src={products[7].image} alt="" />
+          <img className="featured-image" src={products[4].image} alt="" />
+          <button className="featured-btn">Show All</button>
+        </div>
+      )}
     </div>
   );
 }
