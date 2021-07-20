b0cb5e54299a8ac881a6f792e5272fba9545529e
Anurag-Spec <79415746+Anurag-Spec@users.noreply.github.com>
2021-07-21 03:15:13 +0530

    html added to featured

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import Navbar from "./Components/Navbar/Navbar";
 import Carousel from "./Components/Carousel/Carousel";
+import Featured from "./Components/Featured/featured";
 import "./App.css";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Navbar />
       <Carousel />
+      <Featured />
     </div>
   );
 }
---
+++ frontend/src/Components/Featured/featured.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Featured() {
+  return (
+    <div>
+      <h3 className="featured-title">Featured Products</h3>
+      <img className="featured-image" src="#" alt="" />
+      <img className="featured-image" src="#" alt="" />
+      <img className="featured-image" src="#" alt="" />
+      <button className="featured-btn">Show All</button>
+    </div>
+  );
+}
+
+export default Featured;
