56cd8fbc82b558ec41b47672af5cf7aae0f2f9a5
Anurag-Spec <79415746+Anurag-Spec@users.noreply.github.com>
2021-07-26 09:50:54 +0530

    created routes

--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -1,5 +1,7 @@
 import React from "react";
+import Cart from "../Cart/Cart";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import {
   faShoppingCart,
   faUser,
@@ -14,7 +16,9 @@ function Navbar() {
         <div className="nav-title">Dforce Esports </div>
         <ul className="nav-list">
           <li className="nav-list-item">
-            <FontAwesomeIcon icon={faShoppingCart} size="lg" />
+            <Link to="/cart">
+              <FontAwesomeIcon icon={faShoppingCart} size="lg" />
+            </Link>
           </li>
           <li className="nav-list-item">
             <FontAwesomeIcon icon={faUser} size="lg" />
