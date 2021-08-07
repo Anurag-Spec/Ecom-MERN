458078a18c635180eb740e6cb9763d9ebfcd0969
Anurag-Spec <79415746+Anurag-Spec@users.noreply.github.com>
2021-08-07 08:31:38 +0530

    chanegs to signup redux

--- frontend/src/Pages/SignUp/SignUp.js
+++ frontend/src/Pages/SignUp/SignUp.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { Register } from "../../actions/Register";
+
 import "./SignUp.css";
 
 function SignUp() {
