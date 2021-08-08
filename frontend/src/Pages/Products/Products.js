37b1a1ebd526d78f9bd82c71a29c31a870fc7903
Anurag-Spec <79415746+Anurag-Spec@users.noreply.github.com>
2021-08-08 21:31:16 +0530

    updated css for products page

--- frontend/src/Pages/Products/Products.js
+++ frontend/src/Pages/Products/Products.js
@@ -2,6 +2,8 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { listProducts } from "../../actions/actions";
 import "./Products.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart, faHeart } from "@fortawesome/free-solid-svg-icons";
 
 function Products() {
   const dispatch = useDispatch();
@@ -28,8 +30,16 @@ function Products() {
               <div>
                 <img src={product.image} alt="" />
               </div>
-              <div className="card-text ">{product.name}</div>
-              <div className="card-text ">{product.brand}</div>
+              <div className="card-text-sub ">{product.name}</div>
+              <div className="card-text">{product.brand}</div>
+              <div className="card-btn-container">
+                <button className="card-button">
+                  <FontAwesomeIcon icon={faShoppingCart} size="2x" />
+                </button>
+                <button className="card-button">
+                  <FontAwesomeIcon icon={faHeart} size="2x" />
+                </button>
+              </div>
             </div>
           ))}
         </div>
