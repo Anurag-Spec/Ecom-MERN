84c813381690e15f73e59ef11a29c56ee1a8d2fa
Anurag-Spec <79415746+Anurag-Spec@users.noreply.github.com>
2021-08-11 02:11:46 +0530

    created category section for filter

--- frontend/src/Components/filterPage/filterPage.js
+++ frontend/src/Components/filterPage/filterPage.js
@@ -1,8 +1,21 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import "./filterPage.css";
 
 function Filterpage() {
-  return <div className="filter-main">Filterpage</div>;
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
+
+  return (
+    <div className="filter-main">
+      <aside className="filter-option-cont">
+        <div className="filter-option">Category</div>
+        <div className="filter-option">Brand</div>
+        <div className="filter-option">In-stock</div>
+        <div className="filter-option">Reviews</div>
+      </aside>
+    </div>
+  );
 }
 
 export default Filterpage;
