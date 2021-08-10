33e203793807c9679011e07b462651e34aceda5b
Anurag-Spec <79415746+Anurag-Spec@users.noreply.github.com>
2021-08-11 02:34:24 +0530

    changes to filters page

--- frontend/src/Components/filterPage/filterPage.js
+++ frontend/src/Components/filterPage/filterPage.js
@@ -1,19 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import "./filterPage.css";
 
 function Filterpage() {
   const productList = useSelector((state) => state.productList);
   const { loading, error, products } = productList;
+  const [showcat, setShowCat] = useState(false);
+  const [showbrand, setShowBrand] = useState(false);
+  const [showstock, setShowStock] = useState(false);
+  const [showreviews, setShowReviews] = useState(false);
 
   return (
     <div className="filter-main">
       <aside className="filter-option-cont">
-        <div className="filter-option">Category</div>
-        <div className="filter-option">Brand</div>
-        <div className="filter-option">In-stock</div>
-        <div className="filter-option">Reviews</div>
+        <div
+          onClick={() => {
+            setShowCat(!showcat);
+          }}
+          className="filter-option"
+        >
+          Category
+        </div>
+        <div
+          onClick={() => {
+            setShowBrand(!showbrand);
+          }}
+          className="filter-option"
+        >
+          Brand
+        </div>
+        <div
+          onClick={() => {
+            setShowStock(!showstock);
+          }}
+          className="filter-option"
+        >
+          In-stock
+        </div>
+        <div
+          onClick={() => {
+            setShowReviews(!showreviews);
+          }}
+          className="filter-option"
+        >
+          Reviews
+        </div>
       </aside>
+      {showcat && <div>cat</div>}
+      {showbrand && <div>brand</div>}
+      {showstock && <div>Stock</div>}
+      {showreviews && <div>Reviews</div>}
     </div>
   );
 }
