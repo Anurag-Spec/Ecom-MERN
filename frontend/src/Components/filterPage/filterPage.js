de202a80f6466d2793de414f9cd38c91b43d3e0e
Anurag-Spec <79415746+Anurag-Spec@users.noreply.github.com>
2021-08-11 12:13:21 +0530

    created map for categories

--- frontend/src/Components/filterPage/filterPage.js
+++ frontend/src/Components/filterPage/filterPage.js
@@ -4,11 +4,12 @@ import "./filterPage.css";
 
 function Filterpage() {
   const productList = useSelector((state) => state.productList);
-  const { loading, error, products } = productList;
+  const { products } = productList;
   const [showcat, setShowCat] = useState(false);
   const [showbrand, setShowBrand] = useState(false);
   const [showstock, setShowStock] = useState(false);
   const [showreviews, setShowReviews] = useState(false);
+  products.map((product) => console.log(product.Category));
 
   return (
     <div className="filter-main">
@@ -46,10 +47,63 @@ function Filterpage() {
           Reviews
         </div>
       </aside>
-      {showcat && <div>cat</div>}
-      {showbrand && <div>brand</div>}
-      {showstock && <div>Stock</div>}
-      {showreviews && <div>Reviews</div>}
+      <div className="filter-list">
+        {showcat && (
+          <div className="filter-list-item">
+            <h4>Category</h4>
+            {[...new Set(products.map((product) => product.Category))].map(
+              (cat) => (
+                <div>
+                  <input
+                    type="checkbox"
+                    id="category"
+                    name="cateogry"
+                    value="cat"
+                  />
+                  <label for="category"> {cat}</label>
+                </div>
+              )
+            )}
+          </div>
+        )}
+        {showbrand && (
+          <div className="filter-list-item">
+            <h4>Brand</h4>
+            {[...new Set(products.map((product) => product.brand))].map(
+              (brand) => (
+                <div>
+                  <input
+                    type="checkbox"
+                    id="brand"
+                    name="brand"
+                    value="brand"
+                  />
+                  <label for="category"> {brand}</label>
+                </div>
+              )
+            )}
+          </div>
+        )}
+        {showstock && (
+          <div className="filter-list-item">
+            <h4>InStock</h4>
+            {[...new Set(products.map((product) => product.InStock))].map(
+              (stock) => (
+                <div>
+                  <input
+                    type="checkbox"
+                    id="brand"
+                    name="brand"
+                    value="brand"
+                  />
+                  <label for="category"> {stock}</label>
+                </div>
+              )
+            )}
+          </div>
+        )}
+        {showreviews && <div className="filter-list-item">Reviews</div>}
+      </div>
     </div>
   );
 }
