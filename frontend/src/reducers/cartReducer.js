40d52f964a8cc8e4962549a2d2915f5cc857cad1
Anurag-Spec <79415746+Anurag-Spec@users.noreply.github.com>
2021-08-18 12:42:56 +0530

    created getCart redux

---
+++ frontend/src/reducers/cartReducer.js
@@ -0,0 +1,20 @@
+import {
+  ADD_CART_FAIL,
+  ADD_CART_REQUEST,
+  ADD_CART_SUCCESS,
+} from "../constants/cartConstants";
+
+export const getCartReducer = (state = {}, action) => {
+  switch (action.type) {
+    case ADD_CART_REQUEST:
+      return { loading: true };
+    case ADD_CART_SUCCESS:
+      console.log("red", action.payload);
+      return { loading: false, cart: action.payload };
+    case ADD_CART_FAIL:
+      return { loading: false, error: action.payload };
+
+    default:
+      return state;
+  }
+};
