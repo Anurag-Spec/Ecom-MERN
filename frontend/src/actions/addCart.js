1e0b861a4255f6aad2ac5a9332637850a98ec9e6
Anurag-Spec <79415746+Anurag-Spec@users.noreply.github.com>
2021-08-20 12:13:57 +0530

    created remove cart redux

--- frontend/src/actions/addCart.js
+++ frontend/src/actions/addCart.js
@@ -1,12 +1,7 @@
 import axios from "axios";
-import {
-  ADD_CART_FAIL,
-  ADD_CART_REQUEST,
-  ADD_CART_SUCCESS,
-} from "../constants/cartConstants";
+import { ADD_CART_FAIL, ADD_CART_SUCCESS } from "../constants/cartConstants";
 
 export const AddCart = (email, id) => async (dispatch) => {
-  dispatch({ type: ADD_CART_REQUEST, payload: { email, id } });
   try {
     const { data } = await axios.post("http://localhost:5000/api/addCart", {
       email,
